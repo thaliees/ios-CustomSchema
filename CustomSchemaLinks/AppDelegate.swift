ff5b3582c9fda5f5cffa95bf2b9adb2ad4b5f79a
Thalia Aquino <thalia@softwarerb.com>
2020-04-20 11:00:54 -0500

    Project Base

---
+++ CustomSchemaLinks/AppDelegate.swift
@@ -0,0 +1,58 @@
+//
+//  AppDelegate.swift
+//  CustomSchemaLinks
+//
+//  Created by SoftwareRB on 4/16/20.
+//  Copyright © 2020 Thaliees. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        // Determine who sent the URL.
+        let sendingAppID = options[.sourceApplication]
+        print("source application = \(sendingAppID ?? "Unknown")")
+        
+        // Process the URL.
+        guard let components = NSURLComponents(url: url, resolvingAgainstBaseURL: true),
+            let path = components.path,
+            let params = components.queryItems else {
+                print("Invalid URL or path missing")
+                return false
+        }
+        print(path)
+        if let id = params.first(where: { $0.name == "id" })?.value {
+            print("path = \(path)")
+            print("id = \(id)")
+            // Handle of the paths
+            // It is possible to use the switch
+            if path == "product" {
+                presentDetailViewController(id)
+            }
+            return true
+        } else {
+            print("id missing")
+            return false
+        }
+    }
+    
+    func presentDetailViewController(_ id: String) {
+        guard let navigation = window?.rootViewController as? UINavigationController else { return }
+        
+        let detail = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "DetailsStoryboard") as! DetailsViewController
+        detail.idItem = id
+        navigation.modalPresentationStyle = .fullScreen
+        navigation.pushViewController(detail, animated: true)
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,26 @@
+# Custom Schema Link
+[Project created with Xcode V11.4]
+[Deployment Target > 11.0 ]
+
+## Project Base.
+## Documentation
+* [Defining a Custom URL Scheme for Your App](https://developer.apple.com/documentation/uikit/inter-process_communication/allowing_apps_and_websites_to_link_to_your_content/defining_a_custom_url_scheme_for_your_app)
+
+### Configuring the App
+1. Select the "CustomSchemaLinks" project (Navigator menu)
+1. Select the "CustomSchemaLinks" target
+1. Select the "Info" Section
+1. In URL Types Section, press + and add your scheme:
+[ ] Identifier: Your bundle ID
+[ ] URL Schemes: How will your app be identified from another application
+1. Then, select Info.plist file.
+1. Add the next code (select Open As>Source code):
+`<key>LSApplicationQueriesSchemes</key>
+	<array>
+		<string>universalelinks</string>
+	</array>`
+
+* The sheme is similar to YOU_URL_SCHEMES://PATH
+
+### Handle Scheme Links
+The code you need to handle the link (when it's called) is written in AppDelegate.swift file.
